6b28e18d36b0e54b57609a9675010e56fc832f85
lindsay <techells22@gmail.com>
2016-12-20 16:47:15 -0600

    update fcn to allow for dict passed of max/min size

--- bs4_srcset/bs4_srcset.py
+++ bs4_srcset/bs4_srcset.py
@@ -1,10 +1,29 @@
-def get_img_url_from_srcset(img_element, size_wanted_arr):
+def get_img_url_from_srcset(img_element, size_wanted):
     if img_element == '':
         return None
-    
+    min_size_wanted = _get_min_size_wanted(size_wanted)
+    max_size_wanted = _get_max_size_wanted(size_wanted)
+
     srcsets = img_element.split(',')
-    for size_wanted in size_wanted_arr: # will return first one found in sizewanted_arr
-        for srcset in srcsets:
-            img_url, img_size = srcset.strip().split(' ', 2)
-            if img_size == size_wanted:
-                return img_url
+    # for size_wanted in size_wanted_arr: # will return first one found in sizewanted_arr
+    for srcset in srcsets:
+        img_url, img_size = srcset.strip().split(' ', 2)
+        img_size = _convert_size_to_float(img_size)
+        if img_size >= min_size_wanted and img_size <= max_size_wanted:
+            return img_url
+
+def _convert_size_to_float(size_str):
+    size = size_str[:-1] # remove x|w at end
+    return float(size)
+
+def _get_min_size_wanted(size_wanted):
+    if 'min' in size_wanted:
+        return size_wanted['min']
+    else:
+        return 0
+
+def _get_max_size_wanted(size_wanted):
+    if 'max' in size_wanted:
+        return size_wanted['max']
+    else:
+        return 2000
--- tests/bs4_srcset_tests.py
+++ tests/bs4_srcset_tests.py
@@ -5,23 +5,23 @@ from bs4_srcset import bs4_srcset
 
 class UtilsTests(unittest.TestCase):
     def test_get_img_url_from_srcset(self):
-        html = '<td colspan="2" style="text-align:center"><a href="/wiki/File:District-Massachusetts.png" class="image"><img alt="District-Massachusetts.png" src="//upload.wikimedia.org/wikipedia/commons/thumb/9/93/District-Massachusetts.png/150px-District-Massachusetts.png" width="150" height="148" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/9/93/District-Massachusetts.png/225px-District-Massachusetts.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/9/93/District-Massachusetts.png/300px-District-Massachusetts.png 2x" data-file-width="600" data-file-height="593" /></a></td>'
+        html = '<img width="264" height="264" src="https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-264x264.png" class="attachment-executive-image size-executive-image wp-post-image" alt="CEODenise_CampbellSoupCompany2016" srcset="https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-264x264.png 264w, https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-150x150.png 150w, https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-300x300.png 300w, https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-145x145.png 145w, https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016.png 362w" sizes="(max-width: 264px) 100vw, 264px" />'
         soup = BeautifulSoup(html, 'html5lib')
         image_srcset = soup.find('img').get('srcset')
 
-        image_url = bs4_srcset.get_img_url_from_srcset('', ['1.5x'])
+        image_url = bs4_srcset.get_img_url_from_srcset('', {'min':150})
         self.assertEqual(image_url, None)
 
-        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, ['1.5x'])
-        self.assertEqual(image_url, '//upload.wikimedia.org/wikipedia/commons/thumb/9/93/District-Massachusetts.png/225px-District-Massachusetts.png')
+        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, {})
+        self.assertEqual(image_url, 'https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-264x264.png')
 
-        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, ['2x'])
-        self.assertEqual(image_url, '//upload.wikimedia.org/wikipedia/commons/thumb/9/93/District-Massachusetts.png/300px-District-Massachusetts.png')
+        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, {'min': 150, 'max': 150})
+        self.assertEqual(image_url, 'https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-150x150.png')
 
-        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, ['1x', '1.5x'])
-        self.assertEqual(image_url, '//upload.wikimedia.org/wikipedia/commons/thumb/9/93/District-Massachusetts.png/225px-District-Massachusetts.png')
+        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, {'min': 300})
+        self.assertEqual(image_url, 'https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-300x300.png')
 
-        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, ['2x', '1.5x'])
-        self.assertEqual(image_url, '//upload.wikimedia.org/wikipedia/commons/thumb/9/93/District-Massachusetts.png/300px-District-Massachusetts.png')
+        image_url = bs4_srcset.get_img_url_from_srcset(image_srcset, {'max': 200})
+        self.assertEqual(image_url, 'https://www.campbellsoupcompany.com/wp-content/uploads/sites/31/2016/04/CEODenise_CampbellSoupCompany2016-150x150.png')
 
 # python -m unittest discover -s tests -p "*_tests.py"
